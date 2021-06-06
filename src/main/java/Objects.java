def50465fcbc2366d90a2bd0085f28bdb2676c75
Maciej Demucha <maciej.demucha@interia.pl>
2021-06-06 16:14:16 +0200

    commit 2

---
+++ src/main/java/Objects.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author macie
+ */
+abstract class Objects {
+    
+    protected int positionX;
+    protected int positionY;
+    protected int healthPoints;
+    protected int resourceIndex;
+    protected int dropAmount;
+
+    void takeDamage(){
+    
+    }
+}
+
+
+
