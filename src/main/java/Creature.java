2bab3c436bf34228b1b4d6e2f0883be9798262e8
jakulej <55656474+jakulej@users.noreply.github.com>
2021-06-06 18:30:32 +0200

    Update Creature.java

--- src/main/java/Creature.java
+++ src/main/java/Creature.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
-/**
- *
- * @author macie
- */
 abstract class Creature extends Objects {
-    hghnh
+    public Objects findTarget(){
+    }
+    public void moveToTarget(){
+    
+    }
+    public void eat(){
+    
+    }
 }
